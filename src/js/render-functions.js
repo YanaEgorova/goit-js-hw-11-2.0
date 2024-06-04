03b920a2e9e781337ec71f308f6920989a79a44f
Yana <yanaegorova69.3@gmail.com>
2024-06-04 20:49:11 +0300

    +

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -7,10 +7,14 @@ export default function imageTemplate({
   comments,
   downloads,
 }) {
-  //   return `
-  //     <div class="gallery">
-  //     <a href="images/image1.jpg"><img src="images/thumbs/thumb1.jpg" alt="" title=""/></a>
-  //     <a href="images/image2.jpg"><img src="images/thumbs/thumb2.jpg" alt="" title="Beautiful Image"/></a>
-  // </div>
-  //   `;
+  console.log(webformatURL);
+  console.log(largeImageURL);
+  console.log(tags);
+
+  return `
+      <div class="gallery">
+      <a href="${webformatURL}"><img src="${webformatURL}" alt="" title=""/></a>
+      <a href="${largeImageURL}"><img src="${largeImageURL}" alt="" title="Beautiful Image"/></a>
+  </div>
+    `;
 }

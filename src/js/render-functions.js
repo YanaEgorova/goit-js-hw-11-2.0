b9b1184daf7e8b0fe9515bb69be006c7fd0ded62
Yana <yanaegorova69.3@gmail.com>
2024-06-04 21:04:54 +0300

    +

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -10,13 +10,14 @@ export default function imageTemplate({
   console.log(webformatURL);
   console.log(largeImageURL);
   return `
-      <div class="gallery">
-      <a href="${webformatURL}"><img src="${webformatURL}" alt="" title=""/></a>
-      <a href="${largeImageURL}"><img src="${largeImageURL}" alt="" title="Beautiful Image"/></a>
-      <p>Likes<span>${likes}</span></p>
+      <div class="image-block">
+      <a href="${largeImageURL}">
+      <img src="${webformatURL}" alt="" title=""/>
+        <p>Likes<span>${likes}</span></p>
        <p>Views<span>${views}</span></p>
         <p>Comments<span>${comments}</span></p>
          <p>Downloads<span>${downloads}</span></p>
+      </a>
   </div>
     `;
 }
